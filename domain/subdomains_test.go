f35d2fc5ca07ccbb4d2c858817d3e7a9a2f6b960
qtoad <qtoad@qq.com>
2021-03-06 22:20:55 +0800

    modify myxgo-plusplus

--- domain/subdomains_test.go
+++ domain/subdomains_test.go
@@ -1,11 +1,8 @@
 package domain
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"testing"
 )
 
@@ -20,38 +17,38 @@ func TestSubdomains(t *testing.T) {
 	http.ListenAndServe(":9090", hs)
 }
 
-
 func adminHandlerOne(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
 }
 
-func TestServeHTTP(t *testing.T) {
-	r := gin.Default()
-    r.GET(
-		"/:username",
-		func(c *gin.Context) {
-    	uri, ok := c.Get("location")
-	    if !ok {
-	    	c.JSON(500, gin.H{
-	    		"reason": "Location unknown",
-		    })
-	    }
-	    domain := "awesome.io"
-	    if uri.(*url.URL).Host == domain {
-		     // Want to send client to "https://auth.awesome.io/:username"
-		     s := fmt.Sprintf("https://auth.%s/%s", domain, c.Param("username"))
-		     uri, err := url.Parse(s)
-		     if err != nil {
-			    c.JSON(500, gin.H{
-				"reason": "Subdomain is wrong",
-			 })
-		}
-		rp := new(httputil.ReverseProxy)
-		rp.Director = func(req *http.Request) {
-			req.URL = uri
-		}
-		rp.ServeHTTP(c.Writer, c.Request)
-	}
-}
+//
+//func TestServeHTTP(t *testing.T) {
+//	r := gin.Default()
+//    r.GET(
+//		"/:username",
+//		func(c *gin.Context) {
+//    	uri, ok := c.Get("location")
+//	    if !ok {
+//	    	c.JSON(500, gin.H{
+//	    		"reason": "Location unknown",
+//		    })
+//	    }
+//	    domain := "awesome.io"
+//	    if uri.(*url.URL).Host == domain {
+//		     // Want to send client to "https://auth.awesome.io/:username"
+//		     s := fmt.Sprintf("https://auth.%s/%s", domain, c.Param("username"))
+//		     uri, err := url.Parse(s)
+//		     if err != nil {
+//			    c.JSON(500, gin.H{
+//				"reason": "Subdomain is wrong",
+//			 })
+//		}
+//		rp := new(httputil.ReverseProxy)
+//		rp.Director = func(req *http.Request) {
+//			req.URL = uri
+//		}
+//		rp.ServeHTTP(c.Writer, c.Request)
+//	}
+//}
